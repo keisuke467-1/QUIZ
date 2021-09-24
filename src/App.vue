89ceab25ba180d299139a060c8544ffc22017e11
shimonishi <smns467-1@outlook.jp>
2021-09-24 11:21:28 +0900

    作製中

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import eagle from './components/eagle.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // eagle
   }
 }
 </script>
---
+++ src/components/eagle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alert alert-info">
+    <h2 style="color:red">{{ title }}</h2>
+    <p>{{ message }}</p>
+    <div class="p-3 mb-2 bg-warning text-dark">{{ comment }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'eagle',
+  props: {
+    title: String,
+    message: String,
+    comment: String,
+  }
+}
+</script>
